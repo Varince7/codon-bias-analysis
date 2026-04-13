5c3aa08c01daea844584218e56848a57239b76ab
Varince7 <Varince@yahoo.com>
2026-04-13 09:53:42 -0500

    Concurrent 1 part C

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
+
+enum FileType { SPIKE, REPLICASE }
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+    }
+
+    public static void parseFasta(String path, ArrayList<CodonEntry> list, FileType type) throws IOException{
+        File f = new File(path);
+        Scanner infile = new Scanner(f);
+
+        while(infile.hasNext()){
+            String line = infile.nextLine();
+            for (int i = 0; i < line.length() - 3; i += 3){
+
+                String codon = "";
+                for(int k = i; k < i + 3; k++){
+                    codon += line.charAt(k);
+                }
+
+                for(CodonEntry entry: list){
+                    if(codon.equalsIgnoreCase(entry.getSequence())){
+                        switch(type){
+                            case SPIKE -> entry.incrementSpike();
+                            case REPLICASE -> entry.incrementReplicase();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

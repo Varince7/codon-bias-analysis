17f17c1aac743617fe80710833729e5877f8ba20
Varince7 <Varince@yahoo.com>
2026-04-13 20:48:23 -0500

    Updated parseFasta method

--- Main.java
+++ Main.java
@@ -10,22 +10,31 @@ public class Main {
 
     }
 
-    public static void parseFasta(String path, ArrayList<CodonEntry> list, FileType type) throws IOException{
+public static void parseFasta(ArrayList<CodonEntry> list, String path, FileType type) throws IOException
+    {
+        // Open file
         File f = new File(path);
         Scanner infile = new Scanner(f);
 
-        while(infile.hasNext()){
+        while(infile.hasNext())
+        {
+            // Read a line and loop through it three characters at a time
             String line = infile.nextLine();
-            for (int i = 0; i < line.length() - 3; i += 3){
-
+            for(int i = 0; i <= line.length() - 3; i += 3)
+            {
+                // Loop to construct each codon string
                 String codon = "";
-                for(int k = i; k < i + 3; k++){
+                for(int k = i; k < i + 3; k++)
+                {
                     codon += line.charAt(k);
                 }
-
-                for(CodonEntry entry: list){
-                    if(codon.equalsIgnoreCase(entry.getSequence())){
-                        switch(type){
+                // Find codon match in arraylist
+                for (CodonEntry entry : list)
+                {
+                    if (codon.equals(entry.getCodonSequence()))
+                    {
+                        switch (type)
+                        {
                             case SPIKE -> entry.incrementSpike();
                             case REPLICASE -> entry.incrementReplicase();
                         }
@@ -33,5 +42,7 @@ public class Main {
                 }
             }
         }
+        // Close file
+        infile.close();
     }
 }

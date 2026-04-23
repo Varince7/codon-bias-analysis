4f22095d09e49d8cbd798109f8643c130a6d847c
Varince7 <varince@yahoo.com>
2026-04-22 20:35:36 -0500

    More comments

--- Lab 9 Group Project/src/CodonEntry.java
+++ Lab 9 Group Project/src/CodonEntry.java
@@ -1,4 +1,7 @@
 public class CodonEntry {
+    // This is the CodonEntry Class, where all the values needed to create files are stored.
+
+    // Initiate Variables
     private String CodonSequence;
     private String AminoAcid;
     private String Abbreviation;
@@ -9,7 +12,9 @@ public class CodonEntry {
     private double RSCUDifference;
     private double RSCUPercentageDifference;
 
+
     public CodonEntry(String codonsequence, String aminoacid, String abbreviation) {
+        // The Constructor for CodonEntry.
         CodonSequence = codonsequence;
         AminoAcid = aminoacid;
         Abbreviation = abbreviation;
@@ -17,50 +22,62 @@ public class CodonEntry {
 
     public String getCodonSequence() {
         return CodonSequence;
+        // Method for retrieving the CodonSequence Variable.
     }
 
     public String getAminoAcid() {
         return AminoAcid;
+        // Method for retrieving the AminoAcid Variable.
     }
 
     public String getAbbreviation() {
         return Abbreviation;
+        // Method for retrieving the Abbreviation Variable.
     }
 
     public int getCodonReplicaseCount() {
         return CodonReplicaseCount;
+        // Method for retrieving the CodonReplicaseCount Variable.
     }
 
     public int getCodonSpikeCount() {
         return CodonSpikeCount;
+        // Method for retrieving the CodonSpikeCount Variable.
     }
 
     public double getReplicaseRSCU() {
         return ReplicaseRSCU;
+        // Method for retrieving the ReplicaseRSCU Variable.
     }
 
     public double getSpikeRSCU() {
         return SpikeRSCU;
+        // Method for retrieving the SpikeRSCU Variable.
     }
 
     public void setReplicaseRSCU(double replicaseRSCU) {
         ReplicaseRSCU = replicaseRSCU;
+        // Method for setting the ReplicaseRSCU Variable.
     }
 
     public void setSpikeRSCU(double spikeRSCU) {
         SpikeRSCU = spikeRSCU;
+        // Method for setting the CSpikeRSCU Variable.
     }
 
     public double getRSCUDifference() {
         return RSCUDifference;
+        // Method for retrieving the RSCUDifference Variable.
     }
 
     public void setRSCUDifference(double RSCUDifference) {
         this.RSCUDifference = RSCUDifference;
+        // Method for setting the RSCUDifference Variable.
     }
 
     public double getRSCUPercentageDifference() {
         return RSCUPercentageDifference;
+        // Method for retrieving the RSCUPersentageDifference Variable.
     }
 
     public void setRSCUPercentageDifference(double RSCUPercentageDifference) {
@@ -69,13 +86,17 @@ public class CodonEntry {
 
     public void incrementSpikeCount() {
         CodonSpikeCount++;
+        // Increases CodonSpikeCount by 1.
+
     }
 
     public void incrementReplicaseCount() {
         CodonReplicaseCount++;
+        // Increases CodonReplicaseCount by 1.
     }
 
     public String toString() {
         return (CodonSequence+", "+AminoAcid+", "+Abbreviation+", "+ReplicaseRSCU+", "+SpikeRSCU+", "+RSCUDifference);
+        // The ToString method for CodonEntry.
     }
 }
